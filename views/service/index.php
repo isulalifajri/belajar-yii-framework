f38a873154a834ada24105f78b26c9b4a112d100
Isul Alifajri <Isul@VADS.CO.ID>
2026-07-20 14:56:42 +0700

    start project

---
+++ views/service/index.php
@@ -0,0 +1,12 @@
+<div class="container mt-5">
+
+    <h2>Daftar Layanan</h2>
+
+    <ul>
+        <li>Hair Cut</li>
+        <li>Hair Wash</li>
+        <li>Hair Coloring</li>
+        <li>Shaving</li>
+    </ul>
+
+</div>
---
+++ views/site/index.php
@@ -0,0 +1,9 @@
+<div class="container mt-5">
+    <h1>Barbershop</h1>
+
+    <p>Selamat datang di website Barbershop.</p>
+
+    <a href="/index.php?r=service/index" class="btn btn-primary">
+        Lihat Layanan
+    </a>
+</div>
