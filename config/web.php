1124d5f1a7227294b439a0d6c6cf71fccc62ad15
Isul Alifajri <Isul@VADS.CO.ID>
2026-07-20 14:58:58 +0700

    pretty url hide index in middle

--- config/web.php
+++ config/web.php
@@ -26,6 +26,7 @@ $config = [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'rxQTSUfH3lDBDuqwenOM3_2iufqxlFUE',
         ],
+        // this pretty url, langsung menghilangkan index ditengah
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
